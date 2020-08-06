503d9e9a9c33771dc1ca1e636616ab4809f371ab
mar1an4ik <69079587+mar1an4ik@users.noreply.github.com>
2020-08-06 12:59:59 +0300

    Updates

---
+++ precache-manifest.f052585545c96440de8d7025967d68c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64609916b3c5d47ddad3303e36489960",
+    "url": "/Dyplomna/index.html"
+  },
+  {
+    "revision": "e6b553113925348756e3",
+    "url": "/Dyplomna/static/css/main.1768611f.chunk.css"
+  },
+  {
+    "revision": "58dea73784def33b9e1b",
+    "url": "/Dyplomna/static/js/2.c99cdd65.chunk.js"
+  },
+  {
+    "revision": "d2cf8d37946cd02abf2fe73b5ccb09c4",
+    "url": "/Dyplomna/static/js/2.c99cdd65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6b553113925348756e3",
+    "url": "/Dyplomna/static/js/main.2c0e85df.chunk.js"
+  },
+  {
+    "revision": "82f5973be749be779459",
+    "url": "/Dyplomna/static/js/runtime-main.faa3ab0c.js"
+  }
+]);
